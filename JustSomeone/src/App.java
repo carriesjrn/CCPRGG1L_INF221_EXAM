72a1fc59de10481bd13ac3d4f67881bf180404f8
carriesjrn <111479811+carriesjrn@users.noreply.github.com>
2022-09-05 10:01:31 +0800

    Add files via upload

---
+++ JustSomeone/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+       
+         // Student Class
+      Student Czarina = new Student();
+
+     Czarina.surname = "Sajorne";
+     Czarina.firstName = "Czarina";
+     Czarina.middleInitial = 'A';
+     Czarina.dateOfBirth = "01/24/2003";
+     Czarina.studentNumber = "2022102418";
+     Czarina.emailAddress = "sajorneca@students.national-u.edu.ph";
+     Czarina.iAmAwsome = true;
+     Czarina.sayMystudentNumber();
+     Czarina.sayMyemailAddress();
+     Czarina.sayMyiAmAwsome();
+
+    }
+}
---
+++ JustSomeone/src/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+
+     // Student Class
+
+     String surname;
+     String firstName;
+     char middleInitial;
+     String dateOfBirth;
+     String studentNumber;
+     String emailAddress;
+     boolean iAmAwsome;
+
+     // Student Class
+     public void sayMysurname() {
+        System.out.println("My surname is " + surname);
+    }
+    public void sayMyfirstName() {
+            System.out.println("My firstName is" + firstName);
+    }
+    public void sayMymiddleInitial() {
+        System.out.println("My middleIntial is " + middleInitial);
+    }
+    public void sayMydateOfBirth() {
+            System.out.println("My dateOfBirth is " + dateOfBirth);
+    }
+    public void sayMystudentNumber() {
+                System.out.println("My studentNumber is " + studentNumber);
+    }
+    public void sayMyemailAddress() {
+        System.out.println("My emailAddress is " + emailAddress);
+    }
+    public void sayMyiAmAwsome() {
+            System.out.println("Am i Awsome " + iAmAwsome);
+    }
+    
+     }
+
+
